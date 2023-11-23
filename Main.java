26683dd35824eae7fda3c3388ab5c14302b9b7b2
Mehdi-AllaliCr7 <allalim@3il.fr>
2023-11-23 08:53:57 +0100

    depot du tp0 debug

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         // Problème 1
-        int[] numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        /*int[] numbers = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number at index " + i + ": " + numbers[i]);
-        }
+        }*/
 
         // Problème 2
-        double result = calculateValue(5, 0);
+        double result = calculateValue(5, 1);
         System.out.println("Result: " + result);
 
         // Problème 3
         int count = 10;
-        while (count > 0) {
+        while (count < 20) {
             System.out.println("Count: " + count);
             count += 2;
         }
@@ -20,8 +20,8 @@ public class Main {
         // Problème 4
         String message = "Bonjour";
         System.out.println("Longueur du message : " + message.length());
-        char character = message.charAt(10);
-        System.out.println("Caractère à l'indice 10 : " + character);
+        char character = message.charAt(3);
+        System.out.println("Caractère à l'indice 3 : " + character);
 
         // Problème 5
         String str1 = "Hello";
@@ -36,7 +36,6 @@ public class Main {
     // Méthode pour le problème de division par zéro
     public static int calculateValue(int a, int b) {
         return a / b;
-    }
 
     // Méthode pour le problème d'utilisation incorrecte d'une méthode de classe
     public void method() {
